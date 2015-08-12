b007bd4628a833f13316ce1f538169e75bb6b09a
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-12 13:05:07 -0400

    modulo

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,12 +1,7 @@
 /**
  * Created by tanzmitdemtod on 8/12/15.
  */
-//arithmetic operators
+//modulo operator
 
-//area of triangle is half of the width times the height
-
-var width = 4;
-var height = 5;
-var area = .5 * width * height;
-
-console.log(area);
+var remainder = 32 % 10;
+console.log(remainder);
