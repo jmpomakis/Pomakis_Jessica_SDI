c36559e3034a48757eb46c036ec270059ef56ef3
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-12 17:13:29 -0400

    fixed typo in flowchart,  changed 2 to 3 to account for a one cubic yard bag, started writing script for calculator

--- Pomakis_Jessica_Expressions_Assignment/js/script.js
+++ Pomakis_Jessica_Expressions_Assignment/js/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by tanzmitdemtod on 8/12/15.
  */
+//Jessica Pomakis, Expressions Assignment, August 12, 2015, SDI Section 02
+
+var userInput = [length, width, depth]; //this is an array to hold the values for my equation. This calculator will help the user determine how many bags of soil they will need to fill in a plot for their garden.
+
+var length = userInput[0]; //defining the length variable in the array
+var width = userInput[1]; //defining the width variable in the array
+var depth = userInput[2]; //defining the depth variable in the array
