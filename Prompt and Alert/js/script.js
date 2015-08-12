b81deb14b5f71e14c5ca0f420821970b708f50fd
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-12 14:43:28 -0400

    prompt and alert for user input, calculate area of rectangle

---
+++ Prompt and Alert/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tanzmitdemtod on 8/12/15.
+ */
+//prompt and alert
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter the height.");
+var area = width * height;
+
+alert("The area of your rectangle is: " + area + ".");
+//console.log(area);
