8177614db9d9c4ede9c453747751bbdfd31957c6
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-13 16:34:31 -0400

    condition with expression

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -2,14 +2,16 @@
  * Created by tanzmitdemtod on 8/13/15.
  */
 
-//conditional logic - relational expressions
+//conditional logic - with an expressions
 
-var kidHeight = 30;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
+
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift  > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
