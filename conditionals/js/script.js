0d78b0c8a29c8910a8b9464fa64951e217483965
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-13 17:03:08 -0400

    or operator

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -4,13 +4,13 @@
 
 //conditional logic - logical operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
-//if the price of the phone is less than budget AND if paycheck is over 300
+//if the price of the phone is less than budget OR if we won the lottery
 
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery === true){
    console.log("We can buy the phone!");
 }else{
    console.log("No phone for you!");
