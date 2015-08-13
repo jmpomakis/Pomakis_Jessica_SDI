d0b35ca1242bbd23672fcadfcc0706260ee5969e
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-13 16:28:27 -0400

    conditionals screencasts

---
+++ conditionals/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tanzmitdemtod on 8/13/15.
+ */
+
+//conditional logic
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
