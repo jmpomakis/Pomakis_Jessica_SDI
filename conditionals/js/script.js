2f677bfe9989d5aaae3c7b17a084238c48bb574e
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-13 16:38:34 -0400

    else statement

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -2,16 +2,18 @@
  * Created by tanzmitdemtod on 8/13/15.
  */
 
-//conditional logic - with an expressions
+//conditional logic - with an expression
 
-var kidHeight = 47;
+var kidHeight = 49;
 var minHeight = 48;
-var sneakerLift = 2;
 
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift  > minHeight){
+
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}else{ //otherwise, do this
+    console.log("Sorry kid, you've got some growing to do first!");
 }
