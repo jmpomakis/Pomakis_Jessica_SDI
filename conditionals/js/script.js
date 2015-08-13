2c8f70adab43dec46d2b52249264aecca14a8493
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-13 16:58:40 -0400

    and operator

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -2,24 +2,16 @@
  * Created by tanzmitdemtod on 8/13/15.
  */
 
-//conditional logic - with an expression
+//conditional logic - logical operators
 
-var kidHeight = 46;
-var minHeight = 48;
-var wParentHeight = 45; //height of kid with parent
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
+//if the price of the phone is less than budget AND if paycheck is over 300
 
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches in height
-
-if(kidHeight > minHeight){
-    //you can ride
-    console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight ){
-    //you can ride with parent
-    console.log("You can ride, but only with a parent present.");
-
-}else {
-    //sorry you have growing to do
-    console.log("Sorry kid, you've got some growing to do first!");
+if(iPhonePrice < budget && paycheck > 300){
+   console.log("We can buy the phone!");
+}else{
+   console.log("No phone for you!");
 }
