e8165ec9de297389a08bb2f1e5de98beacb38859
jmpomakis <tanzmitdemtod@Jessicas-MacBook-Pro.local>
2015-08-27 19:54:21 -0400

    made a function to generate random numbers

--- Pomakis_Jessica_Functions_Assignment/js/script.js
+++ Pomakis_Jessica_Functions_Assignment/js/script.js
@@ -15,8 +15,23 @@ function numbersValidation(choice) {
 
 
     while (choice === "") {
-        choice = prompt("Please re-enter your choice.\n\nWould you like Powerball or Florida State lottery numbers?");
+        choice = prompt("Oops! Please re-enter your choice.\n\nWould you like Powerball or Florida State lottery numbers?");
 
     }
     return choice;
 } //this function is used to validate that the user has entered their choice, they will get asked again until they type in their response
+
+//random number generation vv
+
+function randomNumberGen(min, max, num){
+
+    var luckyNumbers = []; //creating an empty array that will be filled with the random numbers that the following three lines will generate. we're making sure the numbers will be within a specified range and that they will be whole numbers.
+
+
+    for (var i = 0; i < num; i++) {
+        var randomNum = Math.random() * (max - min) + min;
+        luckyNumbers[i] = Math.round(randomNum);
+    }
+
+    return luckyNumbers;
+} //this function is used to generate the random numbers, which for each option, we'll fill in the min and max and tell the function how many numbers to give us.
